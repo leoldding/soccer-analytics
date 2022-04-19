aa2e3f195864f8d1fe23d56767cd2330b50394ef
leoldding <leo.ld.ding@gmail.com>
2022-04-19 16:15:59 -0400

    added home and away data tables

--- scripts/impala.sql
+++ scripts/impala.sql
@@ -1,4 +1,6 @@
 invalidate metadata ${var:netid}.soccer_analytics;
+invalidate metadata ${var:netid}.home_stats;
+invalidate metadata ${var:netid}.away_stats;
 
 use ${var:netid};
 
@@ -8,6 +10,6 @@ SELECT STDDEV(full_time_home_goals), STDDEV(full_time_away_goals), STDDEV(half_t
 
 SELECT VARIANCE(full_time_home_goals), VARIANCE(full_time_away_goals), VARIANCE(half_time_home_goals), VARIANCE(half_time_away_goals), VARIANCE(home_shots), VARIANCE(away_shots), VARIANCE(home_shots_target), VARIANCE(away_shots_target), VARIANCE(home_fouls), VARIANCE(away_fouls), VARIANCE(home_corners), VARIANCE(away_corners), VARIANCE(home_yellows), VARIANCE(away_yellows), VARIANCE(home_reds), VARIANCE(away_reds) FROM soccer_analytics;
 
+INSERT INTO home_stats SELECT full_time_home_goals, half_time_home_goals, home_shots, home_shots_target, home_fouls, home_corners, home_yellows, home_reds, half_time_result, full_time_result FROM soccer_analytics; 
 
-
-
+INSERT INTO away_stats SELECT full_time_away_goals, half_time_away_goals, away_shots, away_shots_target, away_fouls, away_corners, away_yellows, away_reds, half_time_result, full_time_result FROM soccer_analytics;
