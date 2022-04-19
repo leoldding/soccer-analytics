ef86162824b05e625a14419ce37311502716876e
leoldding <leo.ld.ding@gmail.com>
2022-04-19 16:31:03 -0400

    more small quality of life changes

--- runProject.sh
+++ runProject.sh
@@ -1,15 +1,13 @@
-echo Enter netid:
-read netid
+read -p "Enter netid: " netid
 
 password=`systemd-ask-password --timeout=0 "Enter password:"`
 
-echo Which dataset do you want to analyze?
-echo Enter dataset number:
-echo 1: English Premier League
-echo 2: Spanish La Liga
-echo 3: Italian Serie A
-echo 4: All Leagues
-read league
+echo "Which dataset do you want to analyze?"
+echo "1: English Premier League"
+echo "2: Spanish La Liga"
+echo "3: Italian Serie A"
+echo "4: All Leagues"
+read -p "Dataset Number: " league
 
 ./scripts/datamr.sh $netid $league
 ./scripts/stats.sh $netid $password $league
